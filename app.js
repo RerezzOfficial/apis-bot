e0c37a26e8586707646790b459ad2c03d2a3799e
Rerezz Official <thegamergemu0001@gmail.com>
2024-12-12 19:54:50 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const session = require('express-session');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+// Middleware untuk body parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set EJS sebagai templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware untuk session
+app.use(session({
+    secret: 'secret_key', // ganti dengan key yang lebih aman
+    resave: false,
+    saveUninitialized: true,
+}));
+
+// Routing
+const routes = require('./routes/index');
+app.use('/', routes);
+
+// Menyajikan file statis dari folder public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Menjalankan server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
